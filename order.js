2723a23e3ffc8a326540b8d1730ecd772d7297fc
Alidhillong <ahaiderctn@gmail.com>
2024-07-17 10:08:06 +0500

    Add files via upload

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'History of Asia',
+        productNumber: '145',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'copmuters science',
+        productNumber: '146',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'structure of atoms',
+        productNumber: '147',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
